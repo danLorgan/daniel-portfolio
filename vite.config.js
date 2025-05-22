cbcd137b21af7bde44c74cd2b35a77bb7e4fdd19
danbright2001 <124036628+danbright2001@users.noreply.github.com>
2025-05-22 14:08:52 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],server: {
+  plugins: [react(), tailwindcss()],
+  base: '/daniel-portfolio/',
+  server: {
     host: true,
     port: 5137,
   }
